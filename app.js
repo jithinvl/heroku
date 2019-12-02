b8a978b8dd517f467b0846b2d521a5b79414f9df
jithinvl <57986924+jithinvl@users.noreply.github.com>
2019-12-02 10:13:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require('express');
+//const bodyparser=require('body-parser');
+
+//var bookRouter=require('./src/router/bookrouter')
+var app=express();
+
+// app.use(bodyparser.urlencoded({extended:true}));
+
+// app.set('views','./src/views');
+// app.set('view engine','ejs');
+
+// app.use("/book",bookRouter);
+
+app.get("/",function(req,res)
+{
+ res.send("welcome");
+});
+
+const port=process,env,PORT || 3000
+app.listen(3000,function(){
+    console.log("port 3000 is using");
+});
