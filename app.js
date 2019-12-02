bcd3be13a5055ec61cd5873484c5f658839d9b5e
jithinvl <57986924+jithinvl@users.noreply.github.com>
2019-12-02 10:37:49 +0530

    second

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get("/",function(req,res)
  res.send("welcome");
 });
 
-const port=process,env,PORT || 3000
+const port=process.env.PORT || 3000
 app.listen(3000,function(){
     console.log("port 3000 is using");
 });
